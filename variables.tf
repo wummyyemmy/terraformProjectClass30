395cb09d89171b71c78dac8846ecaf657d7647ba
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-16 00:01:34 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+#variables
+
+variable "bucket_name" {}
+
+variable "acl_value" {
+
+    default = "private"
+
+}
+
+variable "aws_access_key" {
+
+default = “<your_access_key>”
+
+}
+
+variable "aws_secret_key" {
+
+default = “<your_secret_key>”
+
+ }
+
+variable "region" {
+
+    default = "region"
+
+}
