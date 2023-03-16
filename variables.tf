dd75e7cc6b0b1d9f151785c1c0c84b0c6b42bcb9
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-16 02:29:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,39 +1,29 @@
 #variables
 
-variable "bucket_name" {}
+# variable "bucket_name" {}
 
 variable "acl_value" {
-
     default = "private"
 
-}
+} 
 
 variable "AWS_KEY_ID" {
-
-default = “AKIA3B3RFAPWYIWF4XJF”
+    default = "AKIA3B3RFAPWYIWF4XJF"
 
 }
 
 variable "AWS_SECRET" {
-
-default = “mqw8v3XOwxGNk9Cjbx20MQ9gVX9CO7V565l7R6Jj”
+    default = "mqw8v3XOwxGNk9Cjbx20MQ9gVX9CO7V565l7R6Jj"
 
  }
 
 variable "region" {
-
-    default = "us-east-2a"
+    default = "us-east-2"
 
 }
 
 variable "bucket_name" {
-
     default = "bootcamp30-15032023-akinwumi"
 
 }
 
-variable "acl_value" {
-
-    default = "public-read"
-
-}
