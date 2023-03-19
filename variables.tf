62fce84a4871f908e9d17839533917ef0fd6b19a
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 23:20:58 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,20 @@
-#variables. I have edited all the secret information
-# variable "bucket_name" {}
-variable "acl_value" {
-    default = "private"
-} 
-variable "region" {
-    default = "us-east-2"
-}
-variable "s3buketname" {
-    default = "bootcamp30-15032023-akinwumi"
-}
-variable "bucket_name" {
-    default = "bootcamp30-15032023-akinwumi"
-}
-variable "bucket_acl" {
-    default = "bootcamp30-15032023-acl"
+
+variable "name_suffix" {
+  default = "yourName"
 }
 
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "kms_key_description" {
+  default = "KMS key for S3 bucket encryption"
+}
+
+variable "bucket_key" {
+  default = "mybucketkey"
+}
+
+variable "s3_backend_bucket" {
+  default = "mybuckettag"
+}
