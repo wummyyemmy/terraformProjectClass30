6d3b850638b3e74c087760b89948b5565fdc1a5e
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 23:20:28 -0500

    Update variabless.tf

--- variabless.tf
+++ variabless.tf
@@ -9,3 +9,11 @@ variable "region" {
 variable "kms_key_description" {
   default = "KMS key for S3 bucket encryption"
 }
+
+variable "bucket_key" {
+  default = "mybucketkey"
+}
+
+variable "s3_backend_bucket" {
+  default = "mybuckettag"
+}
