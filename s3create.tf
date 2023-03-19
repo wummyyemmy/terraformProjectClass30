a91789e4b6f0442a3d4906098ca62004661abba4
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 22:50:52 -0500

    Update s3create.tf

--- s3create.tf
+++ s3create.tf
@@ -3,21 +3,19 @@
 provider "aws" {
         region = "${var.region}"
 }
-resource "${var.s3buketname}" {
+resource "aws_s3_bucket" "${var.s3buketname}" {
     bucket = "${var.bucket_name}" 
 #   acl = "${var.acl_value}"   
 }
-
 resource "aws_s3_bucket_acl"  "${var.bucket_acl}" {
-  bucket = ${var..bucket 
+  bucket = ${var.s3buketname}.bucket 
   acl    = "${var.acl_value}"
 }
 resource "aws_kms_key" "mykey" {
     deletion_window_in_days = 10
 }
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "myencription" {
-    bucket = aws_s3_bucket.bootcamp30-15032023-akinwumi.bucket  
+    bucket = aws_s3_bucket."${var.s3buketname}".bucket  
     rule {
             apply_server_side_encryption_by_default {
                 kms_master_key_id = aws_kms_key.mykey.arn
