e54fc01a3b928568dfd3a4e4b0ade1ada9314199
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 23:21:32 -0500

    Update s3create.tf

--- s3create.tf
+++ s3create.tf
@@ -1,25 +1,27 @@
 #This creates an s3 bucket with encryption
 
-provider "aws" {
-        region = "${var.region}"
+rovider "aws" {
+  region = var.region
 }
-resource "aws_s3_bucket" "${var.s3buketname}" {
-    bucket = "${var.bucket_name}" 
-#   acl = "${var.acl_value}"   
+
+resource "random_integer" "bucket_suffix" {
+  min = 1000
+  max = 9999
 }
-resource "aws_s3_bucket_acl"  "${var.bucket_acl}" {
-  bucket = ${var.s3buketname}.bucket 
-  acl    = "${var.acl_value}"
+
+resource "aws_kms_key" "${var.bucket_key}" {
+  description = var.kms_key_description
+  deletion_window_in_days = 10
 }
-resource "aws_kms_key" "mykey" {
-    deletion_window_in_days = 10
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "myencription" {
-    bucket = aws_s3_bucket."${var.s3buketname}".bucket  
+
+resource "aws_s3_bucket" "${var.s3_backend_bucket}" {
+  bucket = "bootcamp30-${random_integer.bucket_suffix.result}-${var.name_suffix}"
+  server_side_encryption_configuration {
     rule {
-            apply_server_side_encryption_by_default {
-                kms_master_key_id = aws_kms_key.mykey.arn
-                sse_algorithm = "aws:kms"
-            }
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.${var.bucket_key}.arn
+        sse_algorithm     = "aws:kms"
+      }
     }
-}           
+  }
+}
