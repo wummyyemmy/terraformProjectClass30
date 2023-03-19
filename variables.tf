ccaefecb91d7a01b070fd378256bf486749d2a68
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 22:25:15 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,18 @@
 #variables. I have edited all the secret information
-
 # variable "bucket_name" {}
-
 variable "acl_value" {
     default = "private"
-
 } 
 
 variable "AWS_KEY_ID" {
-    default = "AKIA3WYIWF4XJF"
-
+    default = ""
 }
-
 variable "AWS_SECRET" {
-    default = "mqw8v3V565l7R6Jj"
-
- }
+    default = ""
+}
 
 variable "region" {
     default = "us-east-2"
-
 }
 
 variable "bucket_name" {
