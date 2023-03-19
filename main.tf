d32d9990eac2bc457f9c165c844f23646b7626aa
wummyyemmy <113473594+wummyyemmy@users.noreply.github.com>
2023-03-18 23:16:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,17 @@ resource "random_integer" "bucket_suffix" {
   max = 9999
 }
 
-resource "aws_kms_key" "s3_bucket_key" {
+resource "aws_kms_key" "${var.bucket_key}" {
   description = var.kms_key_description
+  deletion_window_in_days = 10
 }
 
-resource "aws_s3_bucket" "s3_backend_bucket" {
+resource "aws_s3_bucket" "${var.s3_backend_bucket}" {
   bucket = "bootcamp30-${random_integer.bucket_suffix.result}-${var.name_suffix}"
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.s3_bucket_key.arn
+        kms_master_key_id = aws_kms_key.${var.bucket_key}.arn
         sse_algorithm     = "aws:kms"
       }
     }
